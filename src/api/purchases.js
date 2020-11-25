9bad6e87aded3c9d6f7416ff7eb395e4a3db7ae2
TavvyCat <tavish.cary@welch.edu>
2020-11-25 15:29:21 -0600

    Create purchase
    Added create purchase api call, refactored products into Product components, added link for Products

--- src/api/purchases.js
+++ src/api/purchases.js
@@ -5,7 +5,7 @@ export const createPurchase = (purchase, token) => {
   return axios({
     method: 'POST',
     url: `${apiUrl}/purchases`,
-    data: { purchase },
+    data: { purchase: purchase },
     headers: {
       Authorization: `Bearer ${token}`
     }
